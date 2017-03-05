361d8a8190b14f6e582860987d046ea046860819
Jesse Weed <jesse@jesseweed.com>
2017-03-05 15:17:13 -0800

    Cleanup rollup file

--- rollup.config.js
+++ rollup.config.js
@@ -1,51 +1,10 @@
 // LIBRARY
-import { rollup } from './rollup/vue.js'; // VUE
-// import { rollup } from './rollup/react.js'; // REACT
-// import { rollup } from './rollup/riot.js'; // RIOT
+import { vue as rollup } from './rollup/vue.js'; // VUE
+// import { react as rollup } from './rollup/react.js'; // REACT
+// import { riot as rollup } from './rollup/riot.js'; // RIOT
 
-// CONFIG
-import config     from './shared/config.js';
-
-// PLUGINS
-import babel      from 'rollup-plugin-babel';
-import eslint     from 'rollup-plugin-eslint';
-import globals    from 'rollup-plugin-node-globals';
-import replace    from 'rollup-plugin-replace';
-import resolve    from 'rollup-plugin-node-resolve';
-
-// COMMON SETTINGS
-rollup.dest       = '_dist/app.min.js'; // EXPORT FILE
-rollup.format     = 'iife'; // EMPORT FORMAT
-rollup.moduleName = config.shared.name;
-rollup.sourceMap  = true;
-
-// COMMON PLUGINS
-
-rollup.plugins.push(eslint({
-  exclude: [
-    'public/**',
-    'client/**/*.pug',
-    'client/**/*.scss',
-    'client/**/*.styl'
-  ]
-}));
-
-rollup.plugins.push(babel({
-  babelrc: false,
-  exclude: 'node_modules/**',
-  presets: [ [ 'es2015', { modules: false } ], 'stage-0', 'react' ],
-  plugins: [ 'external-helpers' ]
-}));
-
-rollup.plugins.push(resolve({
-  browser: true,
-  jsnext: true,
-  main: true,
-  module: true
-}));
-
-rollup.plugins.push(globals());
-
-rollup.plugins.push(replace({ 'process.env.NODE_ENV': JSON.stringify('development') }));
+// OVERRIDE SETTINGS
+rollup.dest       = '_dist/app.min.js'; // GENERIC EXPORT FILE
+// rollup.sourceMap  = false; // DISABLE SOURCE MAPS
 
 export default rollup;
